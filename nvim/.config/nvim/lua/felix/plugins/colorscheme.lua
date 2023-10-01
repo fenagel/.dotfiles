0e0a83d422d683855bc38cd9758bfef562503bb4
fenagel <felixnagel105@gmail.com>
2023-10-01 15:20:25 +0200

    update

--- nvim/.config/nvim/lua/felix/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/felix/plugins/colorscheme.lua
@@ -119,11 +119,11 @@ return {
 			},
 		},
 	},
-	-- -- Configure LazyVim to load colorscheme
-	-- -- {
-	-- -- 	"LazyVim/LazyVim",
-	-- -- 	opts = {
-	-- -- 		colorscheme = "nightfly",
-	-- -- 	},
-	-- -- },
+	-- Configure LazyVim to load colorscheme
+	{
+		"LazyVim/LazyVim",
+		opts = {
+			colorscheme = "catppuccin",
+		},
+	},
 }
