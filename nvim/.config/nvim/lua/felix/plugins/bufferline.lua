74400618ef539170ce4711d55a6fed9089ea794b
fenagel <felixnagel105@gmail.com>
2023-10-04 12:02:41 +0200

    chore: update

---
+++ nvim/.config/nvim/lua/felix/plugins/bufferline.lua
@@ -0,0 +1,16 @@
+return {
+	"akinsho/bufferline.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	version = "*",
+	opts = {
+		options = {
+       separator_style = { "", "" },
+       offsets = { { text_align = "left", separator = false } },
+       indicator = { style = "none" },
+       show_buffer_close_icons = false,
+       show_close_icon = false,
+       show_tab_indicators = false,
+       always_show_bufferline = false,
+		},
+	},
+}
