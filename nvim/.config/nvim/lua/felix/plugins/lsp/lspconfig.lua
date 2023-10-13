e5daad8ecc3952919abdd0be827bd7cf69258b66
fenagel <felixnagel105@gmail.com>
2023-10-13 14:32:44 +0200

    update

--- nvim/.config/nvim/lua/felix/plugins/lsp/lspconfig.lua
+++ nvim/.config/nvim/lua/felix/plugins/lsp/lspconfig.lua
@@ -236,5 +236,22 @@ return {
 				},
 			},
 		})
+
+		local default_diagnostic_config = {
+			virtual_text = true,
+			update_in_insert = false,
+			underline = true,
+			severity_sort = true,
+			float = {
+				focusable = true,
+				style = "minimal",
+				border = "rounded",
+				source = "always",
+				header = "",
+				prefix = "",
+			},
+		}
+
+		vim.diagnostic.config(default_diagnostic_config)
 	end,
 }
