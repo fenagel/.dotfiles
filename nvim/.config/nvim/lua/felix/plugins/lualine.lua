2e916591c163b60fbde8d0914b76d098db4dcd09
fenagel <felixnagel105@gmail.com>
2023-11-10 20:11:10 +0100

    up

--- nvim/.config/nvim/lua/felix/plugins/lualine.lua
+++ nvim/.config/nvim/lua/felix/plugins/lualine.lua
@@ -57,11 +57,11 @@ return {
 				-- theme = "poimandres",
 				-- theme = "nord",
 				-- theme = "primer_dark",
-				theme = "gruvbox-baby",
+				-- theme = "gruvbox-baby",
 				-- theme = "everforest",
 				-- theme = "gruvbox",
 				-- theme = "nightfox",
-				-- theme = "catppuccin",
+				theme = "catppuccin",
 			},
 			sections = {
 				lualine_x = {
