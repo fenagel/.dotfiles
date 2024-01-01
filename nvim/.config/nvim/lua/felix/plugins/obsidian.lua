64c38d1418f5c516c753bda562966bca5ba00e0f
fenagel <felixnagel105@gmail.com>
2024-01-01 16:06:37 +0100

    chore: update path

--- nvim/.config/nvim/lua/felix/plugins/obsidian.lua
+++ nvim/.config/nvim/lua/felix/plugins/obsidian.lua
@@ -1,4 +1,4 @@
-local path = vim.fn.expand("$HOME") .. "/Documents/Obsidian Vault"
+local path = vim.fn.expand("$HOME") .. "/Library/Mobile Documents/iCloud~md~obsidian/Documents"
 
 return {
 	"epwalsh/obsidian.nvim",
