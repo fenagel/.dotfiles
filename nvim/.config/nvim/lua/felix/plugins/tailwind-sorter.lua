44b26b8eae73cf4b31be6adcf41ce1f0fe3a8953
fenagel <felixnagel105@gmail.com>
2024-01-12 18:45:38 +0100

    chore: update

--- nvim/.config/nvim/lua/felix/plugins/tailwind-sorter.lua
+++ nvim/.config/nvim/lua/felix/plugins/tailwind-sorter.lua
@@ -2,6 +2,7 @@ return {
 	"laytan/tailwind-sorter.nvim",
 	dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-lua/plenary.nvim" },
 	build = "cd formatter && npm i && npm run build",
+	enable = true,
 	opts = {
 		on_save_enabled = true, -- If `true`, automatically enables on save sorting.
 		on_save_pattern = {
