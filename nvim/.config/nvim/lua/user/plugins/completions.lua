0d004545d4cec1d1940aacabf2527da529dc6ad9
fenagel <felixnagel105@gmail.com>
2024-01-15 14:33:50 +0100

    chore: update

--- nvim/.config/nvim/lua/user/plugins/completions.lua
+++ nvim/.config/nvim/lua/user/plugins/completions.lua
@@ -1,67 +1,67 @@
 return {
-	{
-		"hrsh7th/cmp-nvim-lsp",
-	},
-	{
-		"L3MON4D3/LuaSnip",
-		dependencies = {
-			"saadparwaiz1/cmp_luasnip",
-			"rafamadriz/friendly-snippets",
-		},
-	},
-	{
-		"hrsh7th/nvim-cmp",
-		dependencies = {
-			"hrsh7th/cmp-buffer", -- source for text in buffer
-			"hrsh7th/cmp-path", -- source for file system paths
-		"onsails/lspkind.nvim", -- vs-code like pictograms
-		{ "roobert/tailwindcss-colorizer-cmp.nvim", config = true },
-		},
-		config = function()
-			local cmp = require("cmp")
-			local lspkind = require("lspkind")
+  {
+    "hrsh7th/cmp-nvim-lsp",
+  },
+  {
+    "L3MON4D3/LuaSnip",
+    dependencies = {
+      "saadparwaiz1/cmp_luasnip",
+      "rafamadriz/friendly-snippets",
+    },
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-buffer", -- source for text in buffer
+      "hrsh7th/cmp-path",  -- source for file system paths
+      "onsails/lspkind.nvim", -- vs-code like pictograms
+      { "roobert/tailwindcss-colorizer-cmp.nvim", config = true },
+    },
+    config = function()
+      local cmp = require("cmp")
+      local lspkind = require("lspkind")
 
-			require("luasnip.loaders.from_vscode").lazy_load()
+      require("luasnip.loaders.from_vscode").lazy_load()
 
-			cmp.setup({
-				snippet = {
-					expand = function(args)
-						require("luasnip").lsp_expand(args.body)
-					end,
-				},
-				window = {
-					completion = cmp.config.window.bordered(),
-					documentation = cmp.config.window.bordered(),
-				},
-				formatting = {
-					fields = { "menu", "abbr", "kind" },
-					format = lspkind.cmp_format({
-						mode = "symbol_text",
-						maxwidth = 50,
-						ellipsis_char = "...",
-						before = function(entry, vim_item)
-							vim_item = require("tailwindcss-colorizer-cmp").formatter(entry, vim_item)
-							return vim_item
-						end,
-					}),
-				},
-				mapping = cmp.mapping.preset.insert({
-					["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
-					["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
-					["<C-b>"] = cmp.mapping.scroll_docs(-4),
-					["<C-f>"] = cmp.mapping.scroll_docs(4),
-					["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
-					["<C-e>"] = cmp.mapping.abort(), -- close completion window
-					["<CR>"] = cmp.mapping.confirm({ select = false }),
-				}),
-				sources = cmp.config.sources({
-					{ name = "nvim_lsp" },
-					{ name = "luasnip" }, -- For luasnip users.
-				}, {
-					{ name = "buffer" },
-					{ name = "path" }, -- file system paths
-				}),
-			})
-		end,
-	},
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            require("luasnip").lsp_expand(args.body)
+          end,
+        },
+        window = {
+          completion = cmp.config.window.bordered(),
+          documentation = cmp.config.window.bordered(),
+        },
+        formatting = {
+          fields = { "menu", "abbr", "kind" },
+          format = lspkind.cmp_format({
+            mode = "symbol_text",
+            maxwidth = 50,
+            ellipsis_char = "...",
+            before = function(entry, vim_item)
+              vim_item = require("tailwindcss-colorizer-cmp").formatter(entry, vim_item)
+              return vim_item
+            end,
+          }),
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
+          ["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
+          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-f>"] = cmp.mapping.scroll_docs(4),
+          ["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
+          ["<C-e>"] = cmp.mapping.abort(),   -- close completion window
+          ["<CR>"] = cmp.mapping.confirm({ select = false }),
+        }),
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "luasnip" }, -- For luasnip users.
+        }, {
+          { name = "buffer" },
+          { name = "path" }, -- file system paths
+        }),
+      })
+    end,
+  },
 }
--- nvim/.config/nvim/lua/user/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/user/plugins/lsp-config.lua
@@ -1,153 +1,153 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		lazy = false,
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		lazy = false,
-		opts = {
-			auto_install = true,
-		},
-	},
-	{
-		"neovim/nvim-lspconfig",
-		lazy = false,
-		config = function()
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+  {
+    "williamboman/mason.nvim",
+    lazy = false,
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    lazy = false,
+    opts = {
+      auto_install = true,
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    lazy = false,
+    config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-			local lspconfig = require("lspconfig")
-			lspconfig.tsserver.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.html.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.lua_ls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.cssls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.tailwindcss.setup({
-				capabilities = capabilities,
-				filetypes = {
-					"aspnetcorerazor",
-					"astro",
-					"astro-markdown",
-					"blade",
-					"clojure",
-					"django-html",
-					"htmldjango",
-					"edge",
-					"eelixir",
-					"elixir",
-					"ejs",
-					"erb",
-					"eruby",
-					"gohtml",
-					"haml",
-					"handlebars",
-					"hbs",
-					"html",
-					"html-eex",
-					"heex",
-					"jade",
-					"leaf",
-					"liquid",
-					"markdown",
-					"mdx",
-					"mustache",
-					"njk",
-					"nunjucks",
-					"php",
-					"razor",
-					"slim",
-					"twig",
-					"css",
-					"less",
-					"postcss",
-					"sass",
-					"scss",
-					"stylus",
-					"sugarss",
-					"javascript",
-					"javascriptreact",
-					"reason",
-					"rescript",
-					"typescript",
-					"typescriptreact",
-					"vue",
-					"svelte",
-				},
-			})
+      local lspconfig = require("lspconfig")
+      lspconfig.tsserver.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.html.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.cssls.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.tailwindcss.setup({
+        capabilities = capabilities,
+        filetypes = {
+          "aspnetcorerazor",
+          "astro",
+          "astro-markdown",
+          "blade",
+          "clojure",
+          "django-html",
+          "htmldjango",
+          "edge",
+          "eelixir",
+          "elixir",
+          "ejs",
+          "erb",
+          "eruby",
+          "gohtml",
+          "haml",
+          "handlebars",
+          "hbs",
+          "html",
+          "html-eex",
+          "heex",
+          "jade",
+          "leaf",
+          "liquid",
+          "markdown",
+          "mdx",
+          "mustache",
+          "njk",
+          "nunjucks",
+          "php",
+          "razor",
+          "slim",
+          "twig",
+          "css",
+          "less",
+          "postcss",
+          "sass",
+          "scss",
+          "stylus",
+          "sugarss",
+          "javascript",
+          "javascriptreact",
+          "reason",
+          "rescript",
+          "typescript",
+          "typescriptreact",
+          "vue",
+          "svelte",
+        },
+      })
 
-			local util = require("lspconfig.util")
-			local function get_typescript_server_path(root_dir)
-				-- local global_ts = "/home/[yourusernamehere]/.npm/lib/node_modules/typescript/lib"
-				-- Alternative location if installed as root:
-				local global_ts = "/usr/local/lib/node_modules/typescript/lib"
-				local found_ts = ""
-				local function check_dir(path)
-					found_ts = util.path.join(path, "node_modules", "typescript", "lib")
-					if util.path.exists(found_ts) then
-						return path
-					end
-				end
-				if util.search_ancestors(root_dir, check_dir) then
-					return found_ts
-				else
-					return global_ts
-				end
-			end
+      local util = require("lspconfig.util")
+      local function get_typescript_server_path(root_dir)
+        -- local global_ts = "/home/[yourusernamehere]/.npm/lib/node_modules/typescript/lib"
+        -- Alternative location if installed as root:
+        local global_ts = "/usr/local/lib/node_modules/typescript/lib"
+        local found_ts = ""
+        local function check_dir(path)
+          found_ts = util.path.join(path, "node_modules", "typescript", "lib")
+          if util.path.exists(found_ts) then
+            return path
+          end
+        end
+        if util.search_ancestors(root_dir, check_dir) then
+          return found_ts
+        else
+          return global_ts
+        end
+      end
 
-			lspconfig.volar.setup({
-				capabilities = capabilities,
-				filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue", "json" },
-				on_new_config = function(new_config, new_root_dir)
-					new_config.init_options.typescript.tsdk = get_typescript_server_path(new_root_dir)
-				end,
-			})
+      lspconfig.volar.setup({
+        capabilities = capabilities,
+        filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue", "json" },
+        on_new_config = function(new_config, new_root_dir)
+          new_config.init_options.typescript.tsdk = get_typescript_server_path(new_root_dir)
+        end,
+      })
 
-			lspconfig.prismals.setup({
-				capabilities = capabilities,
-			})
+      lspconfig.prismals.setup({
+        capabilities = capabilities,
+      })
 
-			-- configure go language server
-			lspconfig.gopls.setup({
-				capabilities = capabilities,
-			})
+      -- configure go language server
+      lspconfig.gopls.setup({
+        capabilities = capabilities,
+      })
 
-			lspconfig.marksman.setup({
-				capabilities = capabilities,
-			})
+      lspconfig.marksman.setup({
+        capabilities = capabilities,
+      })
 
-			-- configure graphql language server
-			lspconfig.graphql.setup({
-				capabilities = capabilities,
-				filetypes = { "graphql", "gql", "svelte", "typescriptreact", "javascriptreact" },
-			})
+      -- configure graphql language server
+      lspconfig.graphql.setup({
+        capabilities = capabilities,
+        filetypes = { "graphql", "gql", "svelte", "typescriptreact", "javascriptreact" },
+      })
 
-			-- configure emmet language server
-			lspconfig.emmet_ls.setup({
-				capabilities = capabilities,
-				filetypes = { "html", "typescriptreact", "javascriptreact", "css", "sass", "scss", "less", "svelte" },
-			})
+      -- configure emmet language server
+      lspconfig.emmet_ls.setup({
+        capabilities = capabilities,
+        filetypes = { "html", "typescriptreact", "javascriptreact", "css", "sass", "scss", "less", "svelte" },
+      })
 
-			-- configure python server
-			lspconfig.pyright.setup({
-				capabilities = capabilities,
-			})
+      -- configure python server
+      lspconfig.pyright.setup({
+        capabilities = capabilities,
+      })
 
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-			vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
-			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-			vim.keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", { desc = "Show LSP implementations" })
-			vim.keymap.set("n", "<leader>l", vim.diagnostic.open_float, { desc = "Show line diagnostics" })
-		end,
-	},
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+      vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
+      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+      vim.keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", { desc = "Show LSP implementations" })
+      vim.keymap.set("n", "<leader>l", vim.diagnostic.open_float, { desc = "Show line diagnostics" })
+    end,
+  },
 }
