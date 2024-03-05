3c033b31636f870fdf9e3bfd7090433fb451c543
fenagel <felixnagel105@gmail.com>
2024-03-05 08:55:32 +0100

    wip

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -159,9 +159,13 @@ vim.keymap.set({ 'n', 'x', 'o' }, 'L', 'g_', opts)
 -- Replace all instances of highlighted words
 vim.keymap.set('v', '<leader>rr', '"hy:%s/<C-r>h//g<left><left>')
 
--- Move selected line / block of text in visual mode
-vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv", opts)
-vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv", opts)
+-- Move Lines
+vim.keymap.set('n', '<A-j>', '<cmd>m .+1<cr>==', { desc = 'Move down' })
+vim.keymap.set('n', '<A-k>', '<cmd>m .-2<cr>==', { desc = 'Move up' })
+vim.keymap.set('i', '<A-j>', '<esc><cmd>m .+1<cr>==gi', { desc = 'Move down' })
+vim.keymap.set('i', '<A-k>', '<esc><cmd>m .-2<cr>==gi', { desc = 'Move up' })
+vim.keymap.set('v', '<A-j>', ":m '>+1<cr>gv=gv", { desc = 'Move down' })
+vim.keymap.set('v', '<A-k>', ":m '<-2<cr>gv=gv", { desc = 'Move up' })
 
 -- open Netrw
 vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
