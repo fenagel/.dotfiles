48c70abe862f1ca8b521ccf665260f89cf8ce050
fenagel <felixnagel105@gmail.com>
2024-04-29 16:54:12 +0200

    upd

--- nvim/.config/nvim/lua/cfg/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/cfg/plugins/colorscheme.lua
@@ -426,24 +426,53 @@ return {
       vim.api.nvim_command 'colorscheme catppuccin'
     end,
   },
-  {
-    'sainnhe/gruvbox-material',
-    enabled = false,
-    priority = 1000,
-    config = function()
-      vim.g.gruvbox_material_transparent_background = 0
-      vim.g.gruvbox_material_foreground = 'mix'
-      vim.g.gruvbox_material_background = 'hard' -- soft, medium, hard
-      vim.g.gruvbox_material_ui_contrast = 'high' -- The contrast of line numbers, indent lines, etc.
-      vim.g.gruvbox_material_float_style = 'bright' -- Background of floating windows
-      vim.g.gruvbox_material_statusline_style = 'material'
-      vim.g.gruvbox_material_cursor = 'auto'
-
-      -- vim.g.gruvbox_material_colors_override = { bg0 = '#000000' } -- #0e1010
-      -- vim.g.gruvbox_material_colors_override = { bg0 = "#121212" }
-      -- vim.g.gruvbox_material_better_performance = 1
-
-      -- vim.cmd.colorscheme("gruvbox-material")
-    end,
-  },
+  -- {
+  --   'sainnhe/gruvbox-material',
+  --   enabled = true,
+  --   priority = 1000,
+  --   config = function()
+  --     vim.g.gruvbox_material_transparent_background = 0
+  --     vim.g.gruvbox_material_foreground = 'mix'
+  --     vim.g.gruvbox_material_background = 'hard' -- soft, medium, hard
+  --     vim.g.gruvbox_material_ui_contrast = 'high' -- The contrast of line numbers, indent lines, etc.
+  --     vim.g.gruvbox_material_float_style = 'bright' -- Background of floating windows
+  --     vim.g.gruvbox_material_statusline_style = 'material'
+  --     vim.g.gruvbox_material_cursor = 'auto'
+  --
+  --     -- vim.g.gruvbox_material_colors_override = { bg0 = '#000000' } -- #0e1010
+  --     -- vim.g.gruvbox_material_colors_override = { bg0 = "#121212" }
+  --     -- vim.g.gruvbox_material_better_performance = 1
+  --
+  --     vim.cmd.colorscheme 'gruvbox-material'
+  --   end,
+  -- },
+  -- {
+  --   'catppuccin/nvim',
+  --   priority = 150,
+  --   name = 'catppuccin',
+  --   enabled = false,
+  --   config = function()
+  --     require('catppuccin').setup {
+  --
+  --       -- transparent_background = false, -- disables setting the background color.
+  --       -- integrations = {
+  --       --   which_key = true,
+  --       --   cmp = true,
+  --       --   gitsigns = true,
+  --       --   treesitter = true,
+  --       --   notify = false,
+  --       --   telescope = {
+  --       --     enabled = true,
+  --       --     style = "nvchad",
+  --       --   },
+  --       -- },
+  --       background = {
+  --         light = 'latte',
+  --         dark = 'mocha',
+  --       },
+  --     }
+  --
+  --     -- vim.cmd.colorscheme 'catppuccin'
+  --   end,
+  -- },
 }
