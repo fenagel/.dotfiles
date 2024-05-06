a4092ba2e6dda4260e13c2a5bf9bc6034da1ea42
fenagel <felixnagel105@gmail.com>
2024-05-06 18:43:12 +0200

    upd

--- nvim/.config/nvim/lua/cfg/plugins/trouble.lua
+++ nvim/.config/nvim/lua/cfg/plugins/trouble.lua
@@ -2,7 +2,7 @@ return {
   'folke/trouble.nvim',
   dependencies = { 'nvim-tree/nvim-web-devicons', 'folke/todo-comments.nvim' },
   keys = {
-    { '<leader>d', '<cmd>TroubleToggle<CR>', desc = 'Open/close trouble list' },
+    { '<leader>xc', '<cmd>TroubleToggle<CR>', desc = 'Open/close trouble list' },
     { '<leader>xw', '<cmd>TroubleToggle workspace_diagnostics<CR>', desc = 'Open trouble workspace diagnostics' },
     { '<leader>xd', '<cmd>TroubleToggle document_diagnostics<CR>', desc = 'Open trouble document diagnostics' },
     { '<leader>xq', '<cmd>TroubleToggle quickfix<CR>', desc = 'Open trouble quickfix list' },
