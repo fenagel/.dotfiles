5b00049fccf456eb29d50b056cde72f4f50204f4
Felix Nagel <felix@Felixs-MacBook-Pro.local>
2024-08-09 09:41:35 +0200

    update

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -25,6 +25,14 @@ keymap.set("n", "<C-j>", "<Cmd>NvimTmuxNavigateDown<CR>", { silent = true })
 keymap.set("n", "<C-k>", "<Cmd>NvimTmuxNavigateUp<CR>", { silent = true })
 keymap.set("n", "<C-l>", "<Cmd>NvimTmuxNavigateRight<CR>", { silent = true })
 
+keymap.set("n", "<M-h>", '<Cmd>lua require("tmux").resize_left()<CR>', { silent = true })
+keymap.set("n", "<M-j>", '<Cmd>lua require("tmux").resize_bottom()<CR>', { silent = true })
+keymap.set("n", "<M-k>", '<Cmd>lua require("tmux").resize_top()<CR>', { silent = true })
+keymap.set("n", "<M-l>", '<Cmd>lua require("tmux").resize_right()<CR>', { silent = true })
+
+keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
 -- Borderless terminal
 keymap.set("n", "<C-/>", function()
   Util.terminal(nil, { border = "none" })
@@ -39,6 +47,8 @@ end, { desc = "Lazygit (root dir)" })
 keymap.set("n", "ss", ":vsplit<Return>", opts)
 keymap.set("n", "sv", ":split<Return>", opts)
 
+keymap.del({ "n", "i", "v" }, "<A-j>")
+keymap.del({ "n", "i", "v" }, "<A-k>")
 keymap.del("n", "<C-Left>")
 keymap.del("n", "<C-Down>")
 keymap.del("n", "<C-Up>")
