a4af7e05b1af9d5537197338f5ba1219316cb311
Felix Nagel <felix@Felixs-MacBook-Pro.local>
2024-09-12 13:26:15 +0200

    upd

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -20,6 +20,40 @@ vim.api.nvim_create_autocmd("FileType", {
   end,
 })
 
+vim.api.nvim_create_autocmd("ColorScheme", {
+  callback = function()
+    -- change the background color of floating windows and borders.
+    -- vim.cmd('highlight NormalFloat guibg=none guifg=none')
+    -- vim.cmd('highlight FloatBorder guifg=' .. colors.fg .. ' guibg=none')
+    -- vim.cmd('highlight NormalNC guibg=none guifg=none')
+
+    -- vim.cmd('highlight TelescopeBorder guifg=' .. colors.fg .. ' guibg=none')
+    -- vim.cmd('highlight TelescopePromptBorder guifg=' .. colors.fg .. ' guibg=none')
+    -- vim.cmd('highlight TelescopeResultsBorder guifg=' .. colors.fg .. ' guibg=none')
+    --
+    -- vim.cmd('highlight TelescopePromptTitle guifg=' .. colors.fg .. ' guibg=none')
+    -- vim.cmd('highlight TelescopeResultsTitle guifg=' .. colors.fg .. ' guibg=none')
+    -- vim.cmd('highlight TelescopePreviewTitle guifg=' .. colors.fg .. ' guibg=none')
+    --
+
+    -- change neotree background colors
+    -- Default: NeoTreeNormal  xxx ctermfg=223 ctermbg=232 guifg=#d4be98 guibg=#141617
+    -- vim.cmd('highlight NeoTreeNormal guibg=#252e33 guifg=none')
+    -- vim.cmd('highlight NeoTreeFloatNormal guifg=none guibg=none')
+    -- vim.cmd('highlight NeoTreeFloatBorder gui=none guifg=' .. colors.fg .. ' guibg=none')
+    -- vim.cmd('highlight NeoTreeEndOfBuffer guibg=#252e33') -- 1d2021
+
+    vim.api.nvim_set_hl(0, "FloatBorder", { link = "Normal", { bg = "NONE", fg = "NONE" } })
+    vim.api.nvim_set_hl(0, "LspInfoBorder", { link = "Normal" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { link = "Normal", { bg = "NONE", fg = "NONE" } })
+
+    vim.cmd("highlight Winbar guibg=none")
+
+    -- vim.cmd("highlight Comment guifg=#475558")
+    -- vim.cmd("highlight Comment guifg=green")
+  end,
+})
+
 -- attemting to disable terraform ls on fixture file
 -- vim.api.nvim_create_autocmd("BufEnter", {
 --   pattern = "*fixture*",
--- nvim/lua/plugins/disabled.lua
+++ nvim/lua/plugins/disabled.lua
@@ -1,6 +1,6 @@
 return {
   -- { "akinsho/bufferline.nvim", enabled = false },
-  -- { "lukas-reineke/indent-blankline.nvim", enabled = false },
+  { "lukas-reineke/indent-blankline.nvim", enabled = false },
   -- { "folke/noice.nvim", enabled = false },
   -- { "rcarriga/nvim-notify", enabled = false },
   { "echasnovski/mini.indentscope", enabled = false },
