03aaa29f652473ae60fe0343007ff23eb016bbe2
Felix Nagel <felix@Felixs-MacBook-Pro.local>
2024-09-29 17:53:15 +0200

    upd

--- .wezterm.lua
+++ .wezterm.lua
@@ -29,20 +29,21 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices
 
 -- my coolnight colorscheme
-config.colors = {
-	foreground = "#CBE0F0",
-	background = "#16181a",
-	-- background = "#011423",
-	cursor_bg = "#CBE0F0",
-	cursor_border = "#CBE0F0",
-	-- cursor_bg = "#47FF9C",
-	-- cursor_border = "#47FF9C",
-	cursor_fg = "#011423",
-	selection_bg = "#033259",
-	selection_fg = "#CBE0F0",
-	ansi = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#0FC5ED", "#a277ff", "#24EAF7", "#24EAF7" },
-	brights = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#A277FF", "#a277ff", "#24EAF7", "#24EAF7" },
-}
+-- config.colors = {
+-- 	foreground = "#CBE0F0",
+-- 	background = "#16181a",
+-- 	-- background = "#011423",
+-- 	cursor_bg = "#CBE0F0",
+-- 	cursor_border = "#CBE0F0",
+-- 	-- cursor_bg = "#47FF9C",
+-- 	-- cursor_border = "#47FF9C",
+-- 	cursor_fg = "#011423",
+-- 	selection_bg = "#033259",
+-- 	selection_fg = "#CBE0F0",
+-- 	ansi = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#0FC5ED", "#a277ff", "#24EAF7", "#24EAF7" },
+-- 	brights = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#A277FF", "#a277ff", "#24EAF7", "#24EAF7" },
+-- }
+config.color_scheme = "tokyonight_night"
 -- cyberdream theme for wezterm
 -- config.colors = {
 -- 	foreground = "#ffffff",
--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -20,7 +20,7 @@ return { -- Fuzzy Finder (files, lsp, etc)
     { "nvim-telescope/telescope-ui-select.nvim" },
 
     -- Useful for getting pretty icons, but requires a Nerd Font.
-    { "nvim-tree/nvim-web-devicons", enabled = vim.g.have_nerd_font },
+    { "nvim-tree/nvim-web-devicons",            enabled = vim.g.have_nerd_font },
   },
   config = function()
     -- Telescope is a fuzzy finder that comes with a lot of different things that
@@ -95,7 +95,9 @@ return { -- Fuzzy Finder (files, lsp, etc)
     local builtin = require("telescope.builtin")
     vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
     vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
-    vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" })
+    vim.keymap.set("n", "<leader>sf", function()
+      require("config.utils").telescope_git_or_file()
+    end, { desc = "[S]earch [F]iles" })
     vim.keymap.set("n", "<leader>ss", builtin.builtin, { desc = "[S]earch [S]elect Telescope" })
     vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
     vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
